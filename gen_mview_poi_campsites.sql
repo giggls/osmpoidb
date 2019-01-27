45cd150031a851cad13db7f5f1d8d4c396602ce1
Sven Geggus <sven-git@geggus.net>
2019-01-27 20:52:43 +0100

    Add caravan_site

--- gen_mview_poi_campsites.sql
+++ gen_mview_poi_campsites.sql
@@ -13,11 +13,13 @@ CREATE MATERIALIZED VIEW osm_poi_campsites AS
 SELECT    (-1*poly.osm_id)      AS osm_id,
           poly.geom             AS geom,
 -- this will remove the redundant key 'tourism' = 'camp_site' from hstore
-          poly.tags - 'tourism'::text AS tags,
+          poly.tags AS tags,
           'way' as osm_type,
           CASE WHEN poly.tags->'nudism' IN ('yes','obligatory','customary','designated') THEN 'nudist'
                WHEN poly.tags->'group_only' = 'yes' THEN 'group_only'
                WHEN poly.tags->'backcountry' = 'yes' THEN 'backcountry'
+               WHEN ((poly.tags->'tents' = 'yes') AND (poly.tags->'caravans' = 'no')) THEN 'camping'
+               WHEN ((poly.tags->'tents' = 'no') OR ( (poly.tags->'tourism' = 'caravan_site') AND NOT (poly.tags ? 'tents'))) THEN 'caravan'
           ELSE 'standard' END AS category,
           Bool_or(COALESCE(_st_intersects(poly.geom, pt.geom)
 AND       pt.tags->'amenity' = 'telephone', false)) AS telephone,
@@ -60,7 +62,7 @@ AND       (pt.tags ? 'sport') AND (pt.osm_id != poly.osm_id)) THEN pt.tags->'spo
 FROM      osm_poi_poly                               AS poly
 LEFT JOIN osm_poi_all                                AS pt
 ON        poly.geom && pt.geom
-WHERE     (poly.tags ? 'tourism') AND (poly.tags->'tourism' = 'camp_site')
+WHERE     (poly.tags ? 'tourism') AND (poly.tags->'tourism' in ('camp_site','caravan_site'))
 -- campsites from OSM ways
           AND (poly.osm_id < 0) AND (poly.osm_id > -1e17)
 GROUP BY  poly.osm_id,
@@ -70,11 +72,13 @@ UNION ALL
 SELECT    (-1*(poly.osm_id+1e17)) AS osm_id,
           poly.geom               AS geom,
 -- this will remove the redundant key 'tourism' = 'camp_site' from hstore
-          poly.tags - 'tourism'::text - 'type'::text AS tags,
+          poly.tags AS tags,
           'relation' as osm_type,
           CASE WHEN poly.tags->'nudism' IN ('yes','obligatory','customary','designated') THEN 'nudist'
                WHEN poly.tags->'group_only' = 'yes' THEN 'group_only'
                WHEN poly.tags->'backcountry' = 'yes' THEN 'backcountry'
+               WHEN ((poly.tags->'tents' = 'yes') AND (poly.tags->'caravans' = 'no')) THEN 'camping'
+               WHEN ((poly.tags->'tents' = 'no') OR ( (poly.tags->'tourism' = 'caravan_site') AND NOT (poly.tags ? 'tents'))) THEN 'caravan'
           ELSE 'standard' END AS category,
           Bool_or(COALESCE(_st_intersects(poly.geom, pt.geom)
 AND       pt.tags->'amenity' = 'telephone', false)) AS telephone,
@@ -117,7 +121,7 @@ AND       (pt.tags ? 'sport') AND (pt.osm_id != poly.osm_id)) THEN pt.tags->'spo
 FROM      osm_poi_poly                               AS poly
 LEFT JOIN osm_poi_all                                AS pt
 ON        poly.geom && pt.geom
-WHERE     (poly.tags ? 'tourism') AND (poly.tags->'tourism' = 'camp_site')
+WHERE     (poly.tags ? 'tourism') AND (poly.tags->'tourism' in ('camp_site','caravan_site'))
 -- campsites from OSM relations
           AND (poly.osm_id < -1e17)
 GROUP BY  poly.osm_id,
@@ -128,11 +132,13 @@ UNION ALL
 SELECT    osm_id,
           geom,
 -- this will remove the redundant key 'tourism' = 'camp_site' from hstore
-          tags - 'tourism'::text AS tags,
+          tags AS tags,
           'node' as osm_type,
-          CASE WHEN tags->'nudism' IN ('yes','obligatory','customary') THEN 'nudist'
+          CASE WHEN tags->'nudism' IN ('yes','obligatory','customary','designated') THEN 'nudist'
                WHEN tags->'group_only' = 'yes' THEN 'group_only'
                WHEN tags->'backcountry' = 'yes' THEN 'backcountry'
+               WHEN ((tags->'tents' = 'yes') AND (tags->'caravans' = 'no')) THEN 'camping'
+               WHEN ((tags->'tents' = 'no') OR ( (tags->'tourism' = 'caravan_site') AND NOT (tags ? 'tents'))) THEN 'caravan'
           ELSE 'standard' END AS category,
           False,
           False,
@@ -153,7 +159,7 @@ SELECT    osm_id,
           False,
           '{}'
 FROM      osm_poi_point          
-WHERE     ((tags ? 'tourism') AND (tags->'tourism' = 'camp_site'));
+WHERE     (tags ? 'tourism') AND (tags->'tourism' in ('camp_site','caravan_site'));
 
 -- geometry index
 CREATE INDEX osm_poi_campsites_geom ON osm_poi_campsites USING GIST (geom);
