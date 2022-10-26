cfb9c25f534dfa1d944878abd7991c09c19dacb9
Sven Geggus <sven-git@geggus.net>
2022-10-26 22:22:00 +0200

    After update all sites referenced in site relations must be re-build in order to make sure that possible changes to site-relations are respected properly.

--- gen_indexes.sql
+++ gen_indexes.sql
@@ -8,7 +8,9 @@ CREATE INDEX osm_poi_poly_tags_index ON osm_poi_poly USING GIN (tags);
 -- Indexes 4 osm_id (this needs an upstream fix)
 CREATE INDEX osm_poi_point_osm_id_idx ON osm_poi_point USING btree(osm_id);
 CREATE INDEX osm_poi_poly_osm_id_idx ON osm_poi_poly USING btree(osm_id);
-CREATE INDEX osm_poi_line_osm_id_idx ON osm_poi_line USING tree(osm_id);
+CREATE INDEX osm_poi_line_osm_id_idx ON osm_poi_line USING btree(osm_id);
+
+CREATE INDEX osm_poi_camp_siterel_member_id_idx ON osm_poi_camp_siterel USING btree(member_id);
 
 -- Indexes 4 campsites
 CREATE INDEX osm_poi_point_campsite_index ON osm_poi_point USING GIST (geom) WHERE (tags->'tourism') = 'camp_site';
--- post-update.sh
+++ post-update.sh
@@ -11,13 +11,17 @@ t1=$(date +%s)
 psql -q -f post-update.sql $DBNAME
 t2=$(date +%s)
 echo " done in $(expr $t2 - $t1)s."
-echo -n "$(date +'%Y-%m-%d %H:%M:%S')  calling update-poi-campsites-from-siterel.sql..."
-psql -q -f update-poi-campsites-from-siterel.sql $DBNAME
+echo -n "$(date +'%Y-%m-%d %H:%M:%S')  calling clean-opc-from-siterel.sql..."
+psql -q -f clean-opc-from-siterel.sql $DBNAM
 t1=$(date +%s)
 echo " done in $(expr $t1 - $t2)s."
-echo -n "$(date +'%Y-%m-%d %H:%M:%S')  calling update-poi-campsites-with-bugs.sql..."
-psql -q -f update-poi-campsites-with-bugs.sql $DBNAME
+echo -n "$(date +'%Y-%m-%d %H:%M:%S')  calling update-poi-campsites-from-siterel.sql..."
+psql -q -f update-poi-campsites-from-siterel.sql $DBNAME
 t2=$(date +%s)
 echo " done in $(expr $t2 - $t1)s."
+echo -n "$(date +'%Y-%m-%d %H:%M:%S')  calling update-poi-campsites-with-bugs.sql..."
+psql -q -f update-poi-campsites-with-bugs.sql $DBNAME
+t1=$(date +%s)
+echo " done in $(expr $t1 - $t2)s."
 
 
